de3a1c0b81efb7651037c20fd4356e8d35d69f06
leolulu_sad_python <348699103@qq.com>
2025-01-10 19:24:12 +0800

    format code

--- thumbnails_maker.py
+++ thumbnails_maker.py
@@ -685,7 +685,6 @@ def process_video(args, **kwargs):
 
 
 if __name__ == "__main__":
-
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument(
         "video_path",
